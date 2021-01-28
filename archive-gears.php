f518e8a5094fc9f8f3c43dbe3ca07d03d0645c2e
joan parellada <joan.parellada789@gmail.com>
2021-01-28 16:14:14 +0100

    projects

--- archive-gears.php
+++ archive-gears.php
@@ -1,5 +1,21 @@
-<?php if (have_posts()) : while (have_posts()) : the_post(); 
+<?php get_header(); ?>
 
+<div class="page-wrapper black-bg">
+	<div class="nav-menu">
+		
+		<?php wp_nav_menu(); ?>
+	
+	</div>
 
-endwhile; endif;
-?>
+	<div class="content-wrapper">
+		
+	
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+		<?php endwhile; endif; ?>
+
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
--- archive-projects.php
+++ archive-projects.php
@@ -1,5 +1,37 @@
-<?php if (have_posts()) : while (have_posts()) : the_post(); 
+<?php get_header(); ?>
 
+<div class="page-wrapper black-bg">
+	<div class="nav-menu">		
+		<?php wp_nav_menu(); ?>	
+	</div>
 
-endwhile; endif;
-?>
+	<div class="content-wrapper">		
+		<div class="commercial">			
+			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'category_name' => 'commercial', 'posts_per_page' => -1 ) ); 
+			while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				<div class="project-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>					
+				</div>
+			<?php endwhile; ?>
+		</div>
+		<div class="fiction">
+			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'category_name' => 'fiction', 'posts_per_page' => -1 ) ); 
+			while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				<div class="project-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>					
+				</div>
+			<?php endwhile; ?>
+		</div>
+		<div class="videoclip">
+			<?php $loop = new WP_Query( array( 'post_type' => 'projects', 'category_name' => 'videoclip', 'posts_per_page' => -1 ) ); 
+			while ( $loop->have_posts() ) : $loop->the_post(); ?>
+				<div class="project-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>					
+				</div>
+			<?php endwhile; ?>
+		</div>
+	</div>
+
+</div>
+
+<?php get_footer(); ?>
