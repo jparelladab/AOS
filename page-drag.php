f30676cb9a60b4edaa608f9a85cc166c68c9ef9e
joan parellada <joan.parellada789@gmail.com>
2021-02-10 00:12:17 +0100

    page drag and single project

--- page-drag.php
+++ page-drag.php
@@ -2,24 +2,24 @@
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <div class="bg-green">
-  
-  <div class="container-fluid min-height-100 page-drag">
+
+  <div class="container min-height-100 page-drag cursor-black-tiny">
 <!--     <div class="page-drag"> -->
-      
+
       <div class="logo-drag text-xl black poly-regular">A|O|S</div>
 
-        <a style="width:100%;" href="<?php the_permalink(44); ?>">
-          <div class="hold-drag d-flex justify-content-between align-items-center">
+        <a style="width:100%;" href="<?php the_permalink(44); ?>" class="">
+          <div class="hover-black-tiny hold-drag d-flex justify-content-between align-items-center">
             <div class="black poly-bulky text-s">Hold & Drag</div>
               <div class="line mx-4"></div>
             <div class="black poly-bulky text-s">To view content</div>
           </div>
         </a>
 
-        <div class="nav-menu black">
+        <div class="nav-menu black hover-black-tiny text-xs">
           <?php wp_nav_menu(); ?>
         </div>
-<!-- 
+<!--
     </div> -->
   </div>
 </div>
